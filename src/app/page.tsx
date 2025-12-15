68859bab67f8486a6f3d2515f63a960758d71a85
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-15 19:49:33 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[url('/bg.png')] bg-cover bg-center font-sans"></div>
+  );
+}
