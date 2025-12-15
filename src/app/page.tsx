84615c1f70c9b6a45a00bd248770d8db0250c4d0
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-12-15 21:18:11 +0530

    added sidebar and fixed some issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[url('/bg.png')] bg-cover bg-center font-sans"></div>
+    <div className="flex min-h-screen items-center justify-center bg-[url('/bg.png')] bg-contain bg-center bg-no-repeat font-sans"></div>
   );
 }
